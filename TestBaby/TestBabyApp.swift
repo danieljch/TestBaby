86c4a804bf842654fd0bd818bca4feef40c0af64
Daniel Callisaya <danielj.callisayah@gmail.com>
2024-01-24 16:35:44 -0400

    Initial Commit

---
+++ TestBaby/TestBabyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBabyApp.swift
+//  TestBaby
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 24/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TestBabyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
