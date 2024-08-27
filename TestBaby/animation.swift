56734c8fbe2f87ca8dc65af63d2174b9b33de7f9
Daniel Callisaya <danielj.callisayah@gmail.com>
2024-08-26 20:49:20 -0400

    new animation

--- TestBaby/animation.swift
+++ TestBaby/animation.swift
@@ -5,17 +5,19 @@
 //  Created by Daniel Jesus Callisaya Hidalgo on 8/5/24.
 //
 
-import SwiftUI
+iimport SwiftUI
 
 enum SymbolAnimationState {
     case smallToLarge
     case steady
     case largeToSmall
+    case jump
 
     var nextState: SymbolAnimationState {
         switch self {
         case .smallToLarge: return .steady
-        case .steady: return .largeToSmall
+        case .steady: return .jump
+        case .jump: return .largeToSmall
         case .largeToSmall: return .smallToLarge
         }
     }
@@ -23,7 +25,7 @@ enum SymbolAnimationState {
     var scale: CGFloat {
         switch self {
         case .smallToLarge: return 0.2
-        case .steady, .largeToSmall: return 2
+        case .steady, .largeToSmall, .jump: return 2
         }
     }
 }
@@ -34,8 +36,10 @@ struct AnimatedSymbolView: View {
     @Binding var remainingTime: Int
     @State private var state: SymbolAnimationState = .smallToLarge
     @State private var scale: CGFloat = 0.2
+    @State private var yOffset: CGFloat = 0
     private let animationDuration: TimeInterval = 2
     private let steadyDuration: TimeInterval = 2
+    private let jumpDuration: TimeInterval = 0.5
 
     var body: some View {
         VStack {
@@ -44,7 +48,9 @@ struct AnimatedSymbolView: View {
                 .scaledToFit()
                 .frame(width: 100, height: 100)
                 .scaleEffect(scale)
+                .offset(y: yOffset)
                 .animation(.easeInOut(duration: animationDuration), value: scale)
+                .animation(.easeInOut(duration: jumpDuration), value: yOffset)
                 .onAppear {
                     startAnimationCycle()
                 }
@@ -68,6 +74,16 @@ struct AnimatedSymbolView: View {
             scale = state.scale
             state = state.nextState
             DispatchQueue.main.asyncAfter(deadline: .now() + steadyDuration) {
+                remainingTime = 3
+                startAnimationCycle()
+            }
+        case .jump:
+            yOffset = -30
+            DispatchQueue.main.asyncAfter(deadline: .now() + jumpDuration / 2) {
+                yOffset = 0
+            }
+            state = state.nextState
+            DispatchQueue.main.asyncAfter(deadline: .now() + jumpDuration) {
                 remainingTime = 2
                 startAnimationCycle()
             }
